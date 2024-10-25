870b47667b674b2ad117b94352e6e95adf3d2e27
HODUCVU <hoducvu1234@gmail.com>
2024-10-25 13:02:06 +0700

    simulate

--- digital_clock_tb.v
+++ digital_clock_tb.v
@@ -35,10 +35,6 @@ module digital_clock_tb();
 
         #(2*T);
         reset <= 0;
-        #(14*T*4)
-        alarm_en <= 1;
-        #(14*T*4)
-        alarm_en <= 0;
         // modify minutes and hours
         // tick_min <= 1;
         // #(14*T*4)
@@ -46,6 +42,19 @@ module digital_clock_tb();
         // tick_hr <= 1;
         // #(14*T*4)
         // tick_hr <= 0;
+        // set alarm
+        set_alarm <= 1;
+        tick_min <= 1;
+        #(14*T*4)
+        tick_min <= 0;
+        tick_hr <= 1;
+        #(14*T*4)
+        tick_hr <= 0;
+        set_alarm <= 0;
+        // enable alarm
+        alarm_en <= 1;
+        #(10000*T*4*10)
+        alarm_en <= 0;
         #(1000000*T*2); // 200000
         $finish;
     end
